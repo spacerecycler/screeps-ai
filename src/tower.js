c74a25063b4011ab4dacd9b58a27d1d8f3695ca8
spacerecycler <spacerecycler@gmail.com>
2016-07-03 06:12:57 -0700

    See if Object.appy works

--- src/tower.js
+++ src/tower.js
@@ -1,17 +1,19 @@
 var sh = require('shared');
-StructureTower.prototype.doRepair = function() {
-    if(Memory.towers[this.id] == null) {
-        Memory.towers[this.id] = {};
-    }
-    this.doRepair(this.pos, Memory.towers[this.id], function(target) {
-        this.repair(target);
-    });
-};
-StructureTower.prototype.run = function() {
-    this.doRepair();
-    // var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    // if(closestHostile) {
-    //     tower.attack(closestHostile);
-    // }
-};
-StructureTower.prototype.doRepair = sh.doRepair;
+Object.assign(StructureTower.prototype, {
+    run: function() {
+        this.tryRepair();
+        // var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        // if(closestHostile) {
+        //     tower.attack(closestHostile);
+        // }
+    },
+    tryRepair: function() {
+        if(Memory.towers[this.id] == null) {
+            Memory.towers[this.id] = {};
+        }
+        this.doRepair(this.pos, Memory.towers[this.id], function(target) {
+            this.repair(target);
+        });
+    },
+    doRepair: sh.doRepair
+});
