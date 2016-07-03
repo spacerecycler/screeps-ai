3d1debdeba8d9cccd92171234f403718a82e8b66
spacerecycler <spacerecycler@gmail.com>
2016-07-03 06:43:29 -0700

    Don't overwrite everything...

--- src/tower.js
+++ src/tower.js
@@ -1,19 +1,17 @@
 var sh = require('shared');
-StructureTower.prototype = {
-    run: function() {
-        this.tryRepair();
-        // var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        // if(closestHostile) {
-        //     tower.attack(closestHostile);
-        // }
-    },
-    tryRepair: function() {
-        if(Memory.towers[this.id] == null) {
-            Memory.towers[this.id] = {};
-        }
-        this.doRepair(this.pos, Memory.towers[this.id], (target) => {
-            this.repair(target);
-        });
-    },
-    doRepair: sh.doRepair
+StructureTower.prototype.run = function() {
+    this.tryRepair();
+    // var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    // if(closestHostile) {
+    //     tower.attack(closestHostile);
+    // }
 };
+StructureTower.prototype.tryRepair = function() {
+    if(Memory.towers[this.id] == null) {
+        Memory.towers[this.id] = {};
+    }
+    this.doRepair(this.pos, Memory.towers[this.id], (target) => {
+        this.repair(target);
+    });
+};
+StructureTower.prototype.doRepair = sh.doRepair;
