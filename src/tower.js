3e18cc47113da259c940c8ff507a7b9e9d652d46
spacerecycler <spacerecycler@gmail.com>
2016-07-03 18:49:01 -0700

    Fix return states

--- src/tower.js
+++ src/tower.js
@@ -2,19 +2,18 @@ var sh = require('shared');
 StructureTower.prototype.run = function() {
     var closestHostile = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
     if(closestHostile != null) {
-        this.attack(closestHostile);
-        return;
+        if(this.attack(closestHostile) == OK) {
+            return;
+        }
     }
     var damagedCreep = this.pos.findClosestByRange(FIND_MY_CREEPS, {
         filter: (target) => target.hits < target.hitsMax
     });
     if(damagedCreep != null) {
-        this.heal(damagedCreep);
-        return;
+        if(this.heal(damagedCreep) == OK) {
+            return;
+        }
     }
-    this.tryRepair();
-};
-StructureTower.prototype.tryRepair = function() {
     if(Memory.towers[this.id] == null) {
         Memory.towers[this.id] = {};
     }
