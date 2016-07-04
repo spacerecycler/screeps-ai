0d7241a11a50beb1eaa5a088b01cdd5f46f9143a
spacerecycler <spacerecycler@gmail.com>
2016-07-04 11:01:07 -0700

    change order to actually optimize repair

--- src/shared.js
+++ src/shared.js
@@ -14,12 +14,6 @@ var sh = {
         var target = Game.getObjectById(mem.targetId);
         // logic below to only repair things when they are 90% damaged
         // also cap hitpoints for walls since they have so many
-        if(target == null) {
-            target = obj.pos.findNearestHurtStructure();
-            if(target != null) {
-                mem.targetId = target.id;
-            }
-        }
         if(target != null) {
             var max = target.hitsMax;
             if(_.includes([STRUCTURE_WALL,STRUCTURE_RAMPART], target.structureType)) {
@@ -27,12 +21,17 @@ var sh = {
             }
             if(target.hits >= max) {
                 delete mem.targetId;
-            } else {
-                obj.doRepair(target);
-                if(target.hits >= max) {
-                    delete mem.targetId;
-                }
+                target = null;
             }
+        }
+        if(target == null) {
+            target = obj.pos.findNearestHurtStructure();
+            if(target != null) {
+                mem.targetId = target.id;
+            }
+        }
+        if(target != null) {
+            obj.doRepair(target);
         }
         return target;
     }
