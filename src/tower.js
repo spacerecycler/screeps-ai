e3d028628c4cfa93f2b6085a66262a08aa1a3d56
spacerecycler <spacerecycler@gmail.com>
2016-07-03 18:46:40 -0700

    Heal hurt creeps

--- src/tower.js
+++ src/tower.js
@@ -1,8 +1,16 @@
 var sh = require('shared');
 StructureTower.prototype.run = function() {
     var closestHostile = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if(closestHostile) {
-        return this.attack(closestHostile);
+    if(closestHostile != null) {
+        this.attack(closestHostile);
+        return;
+    }
+    var damagedCreep = this.pos.findClosestByRange(FIND_MY_CREEPS, {
+        filter: (target) => target.hits < target.hitsMax
+    });
+    if(damagedCreep != null) {
+        this.heal(damagedCreep);
+        return;
     }
     this.tryRepair();
 };
