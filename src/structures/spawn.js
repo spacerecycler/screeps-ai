5b3f2af5fde412ee390cc1abe332b8c760e5236a
spacerecycler <spacerecycler@gmail.com>
2016-07-22 19:26:20 -0700

    Alter Transport spawn rates

--- src/structures/spawn.js
+++ src/structures/spawn.js
@@ -68,7 +68,11 @@ StructureSpawn.prototype.getExpectedCreeps = function(name) {
         }
         let containerCount = room.getContainerCount();
         if(containerCount > 0 && !room.isMine()) {
-            expected.set(sh.CREEP_TRANSPORTER, Math.min(containerCount, 2));
+            if(room.memory.type == sh.ROOM_KEEPER_LAIR) {
+                expected.set(sh.CREEP_TRANSPORTER, containerCount * 2);
+            } else {
+                expected.set(sh.CREEP_TRANSPORTER, Math.min(containerCount, 2));
+            }
         }
         if(room.energyCapacityAvailable > 0) {
             if(room.energyCapacityAvailable > 400) {
